7333b9ac3606c46e8a4861307b9f6528f38075f6
oggyDB <125797694+oggyDB@users.noreply.github.com>
2024-03-10 12:49:57 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,47 @@
 <html>
+
 <head>
+
 </head>
-<body>
-<font color="blue">
-<font size="6">
-<b>
-    hi guys<br>
-</b>
-</font>
-    <?php echo "hello world today is sunday";?>
+<title>
+php 履歷
+</title>
+
+<body bgcolor=#F3F3FA>
+
+<font size=6 color=#5A5AAD><center><h1><b>
+    方育葳
+</b></h1></center></font>
+<hr color="#C7C7E2"></hr>
+
+<center><img src="..\img\QQQ.jpg"></center>
+<hr color="#C7C7E2"></hr>
+<font size=5 color="#336666"><center><b>
+簡介
+</b></center></font>
+<font size=4 color="#484891"><b><ul>
+<li>就讀學校：高雄大學資訊管理學系</li>
+<li>修過：微積分、行銷管理、資訊管理導論</li>
+<li>擅長：看平板睡著</li>
+<li>證照：機車和汽車駕照</li>
+</ul>
+<font size=5 color="#336666"><center><b>
+自我介紹
+</b></center></font>
+<p>
+我是目前就讀高雄大學資訊管理學系的學生，才剛剛結束可怕的總務地獄，希望之後的必修課可以少用到教科書或原文書，減輕我的負擔；我還希望可以大家都知道自己的本分，讓錢錢可以收齊。<br>
+今年我就會是大三的學生，只能說時間過得真快，不知道沙丘3什麼時候會上映，挺好看的。<br>
+這學期修了電子商務、統計學、網頁設計、資料庫；感覺這學期都是理論課居多，但也不輕鬆，畢竟背的科目不是我的強項，想睡覺的課是還好，可能精神不濟，但通識課偏硬。
+</p>
+<hr color="#C7C7E2"></hr>
+</b></font>
+<center><table width="400"  height="200" border=1>
+    <tr><th>喜好順序</th><th>水果</th><th>專長</th></tr>
+    <tr><td><center>1</center></td><td>蓮霧</td><td>上網找資料</td></tr>
+    <tr><td><center>2</center></td><td>香蕉</td><td>作筆記</td></tr>
+    <tr><td><center>3</center></td><td>香瓜</td><td>安排事務</td></tr>
+</table></center>
+
 </body>
+
 </html>
